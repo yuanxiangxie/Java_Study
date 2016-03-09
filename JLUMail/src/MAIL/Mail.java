e3f09e6c12fdd7dab8181e0fe9602b2d763f9b1f
yuanxiang <yuanxiangxie@outlook.com>
2016-03-09 23:30:16 +0800

    Mail

--- JLUMail/src/MAIL/Mail.java
+++ JLUMail/src/MAIL/Mail.java
@@ -1,18 +1,28 @@
 package MAIL;
 
+import java.util.Vector;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
 public class Mail 
 {
-	private String From;
-	private String To;
-	private String Subject;
-	private String Content;
+	private String From = "yuanxiangboy@163.com";
+	
+	private Vector<String> To;
+	private String Subject = "";
+	private String Content = "";
+	
 	private final String regex = "^[\\w-_]+@([\\w-_]+\\.)+\\w+$";
 	private final Pattern pattern = Pattern.compile(regex);
 	
-	public boolean setFrom(String From)
+	public Mail()
+	{
+		To = new Vector<String>();
+	}
+	
+	
+	private boolean setFrom(String From)
 	{
 		Matcher matcher = pattern.matcher(From);
 		if(matcher.matches())
@@ -24,17 +34,23 @@ public class Mail
 			return false;
 	}
 	
-	public boolean setTo(String To)
+	
+	public Vector<String> setTo(String To)
 	{
-		Matcher matcher = pattern.matcher(To);
-		if(matcher.matches())
+		Vector<String> to = new Vector<String>();
+		String[] to_address = To.split(";");
+		for(int i = 0; i < to_address.length; ++i)
 		{
-			this.To = To;
-			return true;
+			Matcher matcher = pattern.matcher(to_address[i]);
+			if(matcher.matches())
+				this.To.addElement(to_address[i]);
+			else
+				to.addElement(to_address[i]);
 		}
-		else
-			return false;
+		
+		return to;
 	}
+	
 	
 	public void setSubject(String Subject)
 	{
@@ -46,29 +62,37 @@ public class Mail
 		this.Content = Content;
 	}
 	
+	
 	public String getFrom()
 	{
 		return From;
 	}
 	
-	public String getTo()
+	
+	public Vector<String> getTo()
 	{
 		return To;
 	}
 	
-	public String getSubject()
+	
+	private String getSubject()
 	{
 		return Subject;
 	}
 	
-	public String getContent()
+	
+	private String getContent()
 	{
 		return Content;
 	}
 	
-	public static void main(String[] args)
+	public String getMail()
 	{
-		Mail M = new Mail();
-		System.out.println(M.setFrom("yuanxiangboy@@163.com"));
+		String ToWho = "";
+		for(int i = 0; i < getTo().size(); ++i)
+			ToWho += getTo().get(i) + ";";
+			
+		return "From:" + getFrom() + "\r\n" + "To:" + ToWho + "\r\n" + 
+				"Subject:" + getSubject() + "\r\n\r\n" + getContent() + "\r\n";
 	}
 }
